865017e068edd2e98155250b546e74a317b09397
Moitair <gustavosmelo05@gmail.com>
2022-02-04 12:17:33 -0300

    segundo pronto

--- exer2.php
+++ exer2.php
@@ -1,9 +1,15 @@
 <?php
 
-for ($i = 1; $i <= 10; $i++) {
-    
-    echo $i;
-    
+$num = 1;
+
+while($num <= 10){
+    for ($mul = 0; $mul <= 10; $mul++) {
+        
+        echo "$num * $mul =" . $num * $mul . "<br>";
+
+    }
+
+    $num++;
 }
 
 ?>
